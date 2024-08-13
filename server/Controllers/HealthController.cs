062d3b3951b6799e79a21dd84faa70a0b62a9861
Evan Cestnick <evan@cestnick.com>
2024-08-13 01:19:29 -0300

    Added health check route back in to back-end

---
+++ server/Controllers/HealthController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace server.Controllers;
+
+[ApiController]
+[Route("health")]
+public class HealthController : ControllerBase
+{
+    [HttpGet]
+    public IActionResult HealthCheck()
+    {
+        return Ok();
+    }
+}
