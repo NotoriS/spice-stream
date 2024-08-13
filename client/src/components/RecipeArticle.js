6ecce220cd4476d3e99bb5f1446dc7dca910daa3
Evan Cestnick <evan@cestnick.com>
2024-08-12 22:27:49 -0300

    Added functionality to exit an article using the escape key

--- client/src/components/RecipeArticle.js
+++ client/src/components/RecipeArticle.js
@@ -1,14 +1,30 @@
+import { useEffect, useCallback } from "react";
 
 const RecipeArticle = ({ recipe, closeRecipeArticle }) => {
+
+    const escFunction = useCallback((event) => {
+        if (event.key === "Escape") {
+          closeRecipeArticle()
+        }
+    }, [closeRecipeArticle]);
+    
+    useEffect(() => {
+        document.addEventListener("keydown", escFunction, false);
+    
+        return () => {
+          document.removeEventListener("keydown", escFunction, false);
+        };
+    }, [escFunction]);
+
     return (
-      <div className="bg-white p-5 border-b border-gray-100">
-        <div className="flex flex-row justify-between items-center">
-            <h1 className="text-4xl font-bold text-red-600">{recipe.title}</h1>
-            <button onClick={() => closeRecipeArticle()} className=" rounded text-xl hover:bg-neutral-100 py-1 px-3">X</button>
+        <div className="bg-white p-5 border-b border-gray-100">
+            <div className="flex flex-row justify-between items-center">
+                <h1 className="text-4xl font-bold text-red-600">{recipe.title}</h1>
+                <button onClick={() => closeRecipeArticle()} className=" rounded text-xl hover:bg-neutral-100 py-1 px-3">X</button>
+            </div>
+            <p className="text-gray-300 mt-1">2024-08-12 9:05 PM</p>
+            <p>{recipe.description}</p>
         </div>
-        <p className="text-gray-300 mt-1">2024-08-12 9:05 PM</p>
-        <p className="text-gray-700 mt-2">{recipe.description}</p>
-      </div>
     );
   };
   
--- client/src/components/RecipeCard.js
+++ client/src/components/RecipeCard.js
@@ -4,7 +4,7 @@ const RecipeCard = ({ recipe, index, openRecipeArticle }) => {
       <div onClick={() => openRecipeArticle(index)} className="bg-white hover:bg-gray-50 p-5 border-b border-gray-100">
         <h1 className="text-2xl font-bold text-red-600">{recipe.title}</h1>
         <p className="text-gray-300 mt-1">2024-08-12 9:05 PM</p>
-        <p className="text-gray-700 mt-2">{recipe.description}</p>
+        <p>{recipe.description}</p>
       </div>
     );
   };
